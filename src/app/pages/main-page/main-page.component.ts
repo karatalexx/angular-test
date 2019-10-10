18d5d799c12b675775073e99da9f0a65035e38b0
karatalexx <karatalexx@gmail.com>
2019-10-10 16:52:14 +0300

    add: swiper-slider

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
 
 @Component({
   selector: 'app-main-page',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent implements OnInit {
+
+  public config: SwiperConfigInterface = {
+    slidesPerView: 4,
+    spaceBetween: 50,
+    // autoHeight: true,
+    direction: 'horizontal',
+    a11y: true,
+    keyboard: true,
+    mousewheel: true,
+    scrollbar: false,
+    navigation: true,
+    pagination: false,
+  };
 
   constructor() { }
 
--- src/app/pages/main-page/main-page.module.ts
+++ src/app/pages/main-page/main-page.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page.component';
 import { MainPageRoutingModule } from './main-page-routing.module';
+import { MatCardModule } from '@angular/material';
+import { SwiperModule } from 'ngx-swiper-wrapper';
 
 
 
@@ -12,6 +14,8 @@ import { MainPageRoutingModule } from './main-page-routing.module';
   imports: [
     CommonModule,
     MainPageRoutingModule,
-  ]
+    MatCardModule,
+    SwiperModule,
+  ],
 })
 export class MainPageModule { }
