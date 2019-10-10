9635135a9bc7fb2a42bc3dfeee47962d9d601d04
karatalexx <karatalexx@gmail.com>
2019-10-10 14:03:44 +0300

    fix: imporove authMiddleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,20 +2,50 @@ const jwt = require("jsonwebtoken");
 const UserService = require("../service/UserService");
 
 exports.authMiddleware = async function(req, res, next) {
-  const bearerHeader = req.headers.authorization;
-  const token = bearerHeader.split(" ")[1];
-
   try {
-    const decoded = jwt.verify(token, `${process.env.JWT_SECRET}`);
-    if (decoded) {
-      const user = await UserService.getById(decoded.id);
-      req.user = user;
-      return next();
+    // get token from header
+    const bearerHeader = req.headers.authorization;
+    if (!bearerHeader) {
+      return res.status(401).end("Token is not set");
     }
+    const bearerHeaderParts = bearerHeader.split("Bearer ");
+    if (bearerHeaderParts.length < 2) {
+      return res.status(401).end("Bad token");
+    }
+    const token = bearerHeaderParts[1];
+
+    // validate token through JWT
+    const decoded = jwt.verify(
+      token,
+      `${process.env.JWT_SECRET}`,
+      (err, decoded) => {
+        if (err) {
+          return res.status(401).end(err.message);
+        }
+        return decoded;
+      }
+    );
+
+    // check is user exists
+    if (!decoded || !decoded.id) {
+      return res.status(401).end("Bad token");
+    }
+
+    let user = null;
+    try {
+      user = await UserService.getById(decoded.id);
+    } catch (err) {
+      return res.status(401).end("user is not exists");
+    }
+    if (!user) {
+      return res.status(401).end("user is not exists");
+    }
+
+    // OK - go next
+    req.user = user;
+    return next();
   } catch (err) {
     console.error(err);
-    return res.status(500).send(err);
+    return res.status(500).end(err);
   }
-  console.log("usual case when token not pass");
-  res.status(401).send("Bad token");
 };
